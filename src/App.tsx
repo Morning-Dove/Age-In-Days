fd27bff22859aa3bfd9c92954730f6972a62911c
Morning Dove Nelson <morningdhansen@gmail.com>
2024-07-16 11:27:38 -0600

    add calculator component

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,20 @@
 import './App.css'
+import Calculator from "./Calculator";
 
 function App() {
 
   return (
     <>
+      <header>
+        <h1>Age in Days Calculator</h1>
+        <p>This application will take your age in years and return how many days old you are.</p>
+      </header>
+      <main>
+        <Calculator />
+      </main>
+      <footer>
+        <p>Created By: Morning Dove Nelson 2024</p>
+      </footer>
     </>
   );
 }
